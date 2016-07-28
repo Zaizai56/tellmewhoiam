768bb89de11061be6b6557ab7e64d4eb219ba038
Isaie Courilleau <icourilleau@hotmail.fr>
2016-07-28 01:18:57 +0000

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 var port = process.env.PORT || 8080; // set our port
 
 app.listen(port, function() {
